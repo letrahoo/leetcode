0292922e63606ca6e04314b55110d3cb0917019c
letra <letra@bingyan.net>
2018-03-14 01:31:59 +0800

    finish leetcode2_addTwoNum

--- leetcode2_addTwoNum.js
+++ leetcode2_addTwoNum.js
@@ -3,17 +3,13 @@
 // Add the two numbers and return it as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-// Example
-//     Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-//     Output: 7 -> 0 -> 8
-//     Explanation: 342 + 465 = 807.
-
+//Node Constructor
 var ListNode = function(val) {
     this.val = val;
     this.next = null;
-    return this;
 };
 
+//Constructor
 function LinkedList(item) {
     this.head = new ListNode(item);
     this.find = find;
@@ -23,21 +19,22 @@ function LinkedList(item) {
     this.findPre = findPre;
     this.display = display;
     this.displayString = displayString;
-    return this;
 }
+
+var list = new LinkedList(1);
 
 //链表查找
 /**
  * 
- * @param {any} item
+ * @param {number} item
  */
 function find(item) {
     var currentNode = this.head;
     while(currentNode.val !== item) {
-        currentNode = currentNode.next;
         if(currentNode.next === null) {
             return null;
         }
+        currentNode = currentNode.next;
     }
     return currentNode;
 }
@@ -45,11 +42,11 @@ function find(item) {
 //添加元素
 /**
  * 
- * @param {any} iterm 
+ * @param {number} iterm 
  */
 function add(iterm) {
     var currentNode = this.head,
-    newNode = ListNode(iterm);
+    newNode = new ListNode(iterm);
     while(currentNode.next !== null) {
         currentNode = currentNode.next;
     }
@@ -59,11 +56,11 @@ function add(iterm) {
 //链表插入
 /**
  * 
- * @param {any} newElement
- * @param {any} item
+ * @param {number} newElement
+ * @param {number} item
  */
 function insert(newElement, item) {
-    var newNode = ListNode(newElement);
+    var newNode = new ListNode(newElement);
     var target = this.find(item);
     if(target === null) {
         console.log("Insert error!");
@@ -72,6 +69,11 @@ function insert(newElement, item) {
     target.next = newNode;
 }
 
+//链表删除
+/**
+ * 
+ * @param {number} item 
+ */
 function remove(item) {
     var preNode = this.findPre(item);
     if(preNode.next !== null) {
@@ -79,6 +81,7 @@ function remove(item) {
     }
 }
 
+//查找链表前一节点
 function findPre(item) {
     var currentNode = this.head;
     while(!( currentNode.next == null) && ( currentNode.next.val != item )) {
@@ -87,17 +90,31 @@ function findPre(item) {
     return currentNode;
 }
 
+//打印链表
 function display() {
     var currentNode = this.head;
+    var printStr = currentNode.val.toString();
     while ( currentNode.next !== null ){
-        console.log( currentNode.next.element + "->");
+        printStr += ("->" + currentNode.next.val.toString());
         currentNode = currentNode.next;
     }
+    console.log(printStr);
 }
 
 function displayString() {
-
+    var currentNode = this.head;
+    var printStr = currentNode.val.toString();
+    while ( currentNode.next !== null ){
+        printStr += ("->" + currentNode.next.val.toString());
+        currentNode = currentNode.next;
+    }
+    return printStr;
 }
+
+// Example
+//     Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//     Output: 7 -> 0 -> 8
+//     Explanation: 342 + 465 = 807.
 
  /**
  * @param {ListNode} l1
@@ -105,10 +122,32 @@ function displayString() {
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
-};
+    var list = new ListNode(0);
+    var result = list;
+    var sum, carry = 0;
+    while(l1 || l2 || carry) {
+        sum = 0;
+        if(l1 !== null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if(l2 !== null ) {
+             sum += l2.val;
+             l2 = l2.next;
+        }
 
-var list = LinkedList(2);
-list.add(4);
-list.add(5);
-console.log(list.head.next.val);
+        sum += carry;
+        list.next = new ListNode(sum % 10);
+        carry = parseInt(sum / 10);
+        list = list.next;
+    }
+    return result.next;
+};
+var l1 = new LinkedList(2),
+    l2 = new LinkedList(5);
+l1.add(4);
+l1.add(3);
+l2.add(6);
+l2.add(4);
+console.log(l1.head, l2.head);
+console.log(addTwoNumbers(l1.head, l2.head));
